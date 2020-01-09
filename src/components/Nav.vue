03432540bfc5ee525576bf0d9d7e565db28658ca
ricegood <yeonjaekoo@gmail.com>
2020-01-09 16:55:33 +0900

    Responsive nav bar

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -3,10 +3,10 @@
         <nav class="uk-navbar-container uk-navbar-transparent" uk-navbar>
             <div class="uk-light uk-navbar uk-animation-toggle" tabindex="0">
 	      <router-link to="/" style="text-decoration: none;">
-	        <a class="uk-navbar-item uk-logo uk-animation-fade" href="#" style="font-family:'Roboto Mono',monospace; font-size:xx-large; font-weight:700;">VMOlab.</a>
+	        <a class="uk-navbar-item uk-logo uk-animation-fade" href="#" style="padding:0 0 0 0;font-family:'Roboto Mono',monospace; font-size:xx-large; font-weight:700;">VMOlab.</a>
 	      </router-link>
 	    </div>
-            <div class="uk-light uk-navbar-right">
+            <div class="uk-light uk-navbar-right uk-visible@m">
                 <ul class="uk-navbar-nav">
                     <li>
                         <router-link to="/people">People</router-link>
@@ -23,13 +23,15 @@
                     </li>
                     <li>
                         <router-link to="/research">Research</router-link>
+			<!--
                         <div class="uk-navbar-dropdown">
                             <ul class="uk-nav uk-navbar-dropdown-nav">
-                                <li><a href="#">Software</a></li>
-                                <li><a href="#">Demo</a></li>
-                                <li><a href="#">Technical Report</a></li>
+                                <li><router-link to="/research">Software</router-link></li>
+                                <li><router-link to="/research">Demo</router-link></li>
+                                <li><router-link to="/research">Technical Report</router-link></li>
                             </ul>
                         </div>
+			-->
                     </li>
                     <li><router-link to="/publication">Publication</router-link></li>
                     <li><router-link to="/news">News</router-link></li>
@@ -37,6 +39,34 @@
                     <li><router-link to="/overview" uk-icon="icon: album"></router-link></li>
                 </ul>
             </div>
+            <div class="uk-navbar-right uk-hidden@m">
+                <a class="uk-navbar-toggle" uk-navbar-toggle-icon type="button" uk-toggle="target: #offcanvas-nav-primary"></a>
+            </div>
         </nav>
+        <div id="offcanvas-nav-primary" uk-offcanvas="overlay: true; flip: true">
+            <div class="uk-offcanvas-bar uk-flex uk-flex-column">
+                <ul class="uk-nav uk-nav-primary uk-nav-center uk-margin-auto-vertical">
+                    <li><router-link to="/people" class="uk-offcanvas-close">People</router-link></li>
+                    <li>
+                        <router-link to="/research" class="uk-offcanvas-close">Research</router-link>
+			<!--
+                        <ul class="uk-nav-sub">
+                            <li><router-link to="/research" class="uk-offcanvas-close">Software</router-link></li>
+                            <li><router-link to="/research" class="uk-offcanvas-close">Demo</router-link></li>
+                            <li><router-link to="/research" class="uk-offcanvas-close">Technical Report</router-link></li>
+                        </ul>
+			-->
+                    </li>
+                    <li><router-link to="/publication" class="uk-offcanvas-close">Publication</router-link></li>
+                    <li><router-link to="/news" class="uk-offcanvas-close">News</router-link></li>
+                    <li><router-link to="/contact" class="uk-offcanvas-close">Contact</router-link></li>
+                    <li class="uk-nav-divider"></li>
+		    <ul class="uk-nav-sub">
+                        <li><router-link to="/overview" class="uk-offcanvas-close"><span class="uk-margin-small-right" uk-icon="icon: thumbnails"></span> See blocks</router-link></li>
+		    </ul>
+                </ul>
+
+            </div>
+        </div>
   </div>
 </template>
