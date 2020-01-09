be726383e91120f1d462073a21621e112891450a
ricegood <yeonjaekoo@gmail.com>
2020-01-09 16:21:09 +0900

    Resolve Error

--- src/views/News.vue
+++ src/views/News.vue
@@ -1,7 +1,7 @@
 <template>
 <div>
   <!-- Iterate recent 5 years -->
-  <div v-for="i in 5">
+  <div v-bind:v-for="i in 5">
     <h1 class="uk-light">{{currentYear+1-i}}</h1>
     <List category="news" :year="currentYear+1-i" type="A"/>
   </div>
